ed9f8d3a85110e790374eb7e166098f9fb742f51
David Anderson <dander.11.9@gmai.com>
2024-01-15 11:39:57 -0500

    Quartz sync: Jan 15, 2024, 11:39 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dander11.github.io/published-notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
