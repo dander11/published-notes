6d0c4f86b4770659ec91ce874e553dd17e7a886b
David Anderson <dander.11.9@gmai.com>
2024-01-16 20:08:11 -0500

    Quartz sync: Jan 16, 2024, 8:08 PM

--- quartz/components/FrontMatter.tsx
+++ quartz/components/FrontMatter.tsx
@@ -3,18 +3,21 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 export default (() => {
     function FrontMatter({fileData}: QuartzComponentProps) {
-      return (
-        <div>
-          <h2>Front Matter</h2>
-          <ul>
-            {Object.entries(fileData).map(([key, value]) => (
-              <li key={key}>
-                {key}: {value}
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
+        const tags = fileData.frontmatter?.tags;
+        if (!tags) return null;
+  
+        return (
+          <div>
+            <h2>Tags</h2>
+            <ul>
+              {tags.map((tag: string, index: number) => (
+                <li key={index}>
+                  <strong>{tag}</strong>
+                </li>
+              ))}
+            </ul>
+          </div>
+        );
     }
    
    
